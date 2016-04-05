2af1b82e1fd1a387a8ece8a0af28e6dd9d2e9342
David Stinnette <dastinnette@gmail.com>
2016-04-04 18:52:21 -0600

    Moved logic out of CanvasMotion.prototype boat move methods and currently passing move boat left and right happy path tests

--- test/canvas-motion-test.js
+++ test/canvas-motion-test.js
@@ -1,31 +1,34 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const Boat = require('../lib/boat');
+const CanvasMotion = require('../lib/canvas-motion')
+const Boat         = require('../lib/boat');
+const canvasWidth  = 1000
+const canvasHeight = 700
 
 describe('CanvasMotion', function() {
-  var boat = new Boat({});
-  
-  context('moves horizontally', function() {
+  var canvasMotion = new CanvasMotion(canvasWidth, canvasHeight);
+  var boat = new Boat({x: canvasWidth/2, y: 25, width: 200, height: 150, velocity: 5, score: 0});
+
+  context('boat moves horizontally', function() {
     it ('should move boat left', function() {
       var initialPosition = boat.x
       assert.equal(boat.x, boat.x);
 
-      boat.moveBoatLeft(boat);
+      canvasMotion.moveBoatLeft(boat);
       assert.equal(boat.x, initialPosition - 5);
 
-      boat.moveBoatLeft(boat);
+      canvasMotion.moveBoatLeft(boat);
       assert.equal(boat.x, initialPosition - 10);
     });
 
     it ('should move boat right', function() {
       var initialPosition2 = boat.x;
-      var canvasWidth = 700;
 
-      boat.moveBoatRight(boat, canvasWidth);
+      canvasMotion.moveBoatRight(boat, canvasWidth);
       assert.equal(boat.x, initialPosition2 + 5);
 
-      boat.moveBoatRight(boat, canvasWidth);
+      canvasMotion.moveBoatRight(boat, canvasWidth);
       assert.equal(boat.x, initialPosition2 + 10);
     });
 
@@ -33,7 +36,6 @@ describe('CanvasMotion', function() {
       var customBoat = new Boat({x: 15, y: 2, width: 3, height: 4, velocity: 5}, boat);
 
       var initialPosition2 = customBoat.x;
-      var canvasWidth = 700;
 
       customBoat.moveBoatLeft(customBoat);
       assert.equal(customBoat.x, initialPosition2);
@@ -43,7 +45,6 @@ describe('CanvasMotion', function() {
       var customBoat = new Boat({x: 690 , y: 2, width: 10, height: 4, velocity: 5}, boat);
 
       var initialPosition2 = customBoat.x;
-      var canvasWidth = 700;
 
       customBoat.moveBoatRight(customBoat, canvasWidth);
       assert.equal(customBoat.x, initialPosition2);
