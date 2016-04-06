56b97fd9099c7069be6721202457646c95943f0b
Steve Pentler <stevepentler@gmail.com>
2016-04-06 00:15:25 -0600

    Console.logs amplitude and retains functionality

--- sketch.js
+++ sketch.js
@@ -18,15 +18,13 @@ function setup() {
 
 function draw() {
   level = amplitude.getLevel();
-  var size = map(level, 0, 1, 1, 2);
+  var size = map(level, 0, 1, 1, 3);
   count++;
   if (count === 1) {
     amplitudeLevel = createElement('span', size).addClass('amplitudeLevel');
-    console.log("size:" + size);
   } else {
-    debugger
     amplitudeLevel.html('');
     amplitudeLevel = createElement('span', size).addClass('amplitudeLevel');
-    console.log("size:" + size);
+    // console.log("size:" + size);
   }
 }
